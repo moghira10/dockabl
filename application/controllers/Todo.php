0396d5c68e9ce807784da68c33fd28b961efa669
moghira10 <get2moghira@live.com>
2017-08-10 16:54:23 +0530

    resp fixes

--- application/controllers/Todo.php
+++ application/controllers/Todo.php
@@ -11,43 +11,49 @@ class Todo extends CI_Controller {
 
 	public function index()
 	{
+		$taskResp = '';
 		$query = $this->db->query('SELECT todo FROM dock_todo');
+		$response = $query->result();
+		if(!empty($response)) {
+			foreach ($response as $index => $task) {
+				$tasktodo = ($index+1)  . ". " . (string)$task->todo .PHP_EOL;
+				$taskResp .= $tasktodo;
+			}
+		}
 		return $this->output
 			->set_content_type('application/json')
 			->set_status_header(200)
-			->set_output(json_encode(array(
-				$query->result()
-			)));
+			->set_output($taskResp);
 	}
 
 	public function mark()
 	{
-		$params = $this->security->xss_clean($this->input->get());
-		if(!empty($params['task'])) {
-			$response = $this->db->query('DELETE FROM dock_todo WHERE `todo`="' . $params['task'] . '"');
+		$params = $this->security->xss_clean(($this->uri->segment(3)));
+		if(!empty($params)) {
+			$response = $this->db->query('DELETE FROM dock_todo WHERE `todo`="' . $params . '"');
 		}
 		if($response) {
 			return $this->output
 				->set_content_type('application/json')
 				->set_status_header(200)
-				->set_output(json_encode(array(
+				->set_output(
 					"Task Marked Sucessfully!"
-				)));
+				);
 		}
 	}
 	public function add()
 	{
-		$params = $this->security->xss_clean($this->input->get());
-		if(!empty($params['task'])) {
-			$response = $this->db->query('INSERT INTO dock_todo SET `todo`="' . $params['task'] . '"');
+		$params = $this->security->xss_clean(($this->uri->segment(3)));
+		if(!empty($params)) {
+			$response = $this->db->query('INSERT INTO dock_todo SET `todo`="' . $params . '"');
 		}
 		if($response) {
 			return $this->output
 				->set_content_type('application/json')
 				->set_status_header(200)
-				->set_output(json_encode(array(
+				->set_output(
 					"Task Added Sucessfully!"
-				)));
+				);
 		}
 	}
 
